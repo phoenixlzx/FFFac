618b67a26d6f6a9bea369ab52515b6fda7d52f44
Touhou zozyo <zzy.touhou@gmail.com>
2013-11-08 02:10:43 +0800

    more animate 1

--- page_dev/js/functions.js
+++ page_dev/js/functions.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-	/* Long to Short  index.ver */
+/* Long to Short  index.ver */
 	var fadeshort = function() {
 		$('#fireshout span:last').fadeIn("fast");
 	}
@@ -10,40 +10,72 @@ $(document).ready(function() {
 	var selectshort = function() {
 		$('#shorturl').select();
 	}
+
 	$("#fireshout").click(function() {
 		if ($('#fireshout span:last').attr("style") === "display: none;") 
 			{
 				$("#shorturl").animate({width: "450px"});
 				$("#fireshout").animate({width: "200px"},'normal',selectshort);
 				$('#fireshout span:first').fadeOut("fast", fadeshort);
-				$('#shorturl').attr('readonly', 'readonly').val('http://fff.ac/fire').mouseover(selectshort);
+				$('#shorturl').attr('readonly', 'readonly').val('http://fff.ac/fire').click(selectshort);
 				// ajax 
 		} 
 		else {
-			$("#shorturl").animate({
-				width: "820px"
-			});
-			$("#fireshout").animate({
-				width: "450px"
-			});
+			$("#shorturl").animate({width: "820px"});
+			$("#fireshout").animate({width: "450px"});
 			$('#fireshout span:last').fadeOut("fast", fadelong);
 			$('#shorturl').attr('placeholder', '烧！烧！烧！').removeAttr("readonly").val(null);
 		}
 	});
-	/* Long to Short  special.ver */
-		//not ready
-	/* Fire message */
+
+/* Long to Short  special.ver */
+	var fadeshortsp = function() {
+		$('#fireshoutsp span:last').fadeIn("fast");
+	}
+	var fadelongsp = function() {
+		$('#fireshoutsp span:first').fadeIn("fast");
+	}
+	var selectshortsp = function() {
+		$('#shorturlsp').select();
+	}
+
+	$("#fireshoutsp").click(function() {
+		if ($('#fireshoutsp span:last').attr("style") === "display: none;") 
+			{
+				$("#shorturlsp").animate({width: "450px"});
+				$("#fireshoutsp").animate({width: "200px"},'normal',selectshortsp);
+				$('#fireshoutsp span:first').fadeOut("fast", fadeshortsp);
+				$('#shorturlsp').attr('readonly', 'readonly').val('http://fff.ac/firesp').click(selectshortsp);
+				// ajax 
+		} 
+		else {
+			$("#shorturlsp").animate({width: "820px"});
+			$("#fireshoutsp").animate({width: "450px"});
+			$('#fireshoutsp span:last').fadeOut("fast", fadelongsp);
+			$('#shorturlsp').attr('placeholder', 'F！F！F！').removeAttr("readonly").val(null);
+		}
+	});
+
+/* about */
+
+	$('div#wrap div#about').animate({height: '155px'},"fast").fadeIn(600).animate({height: '720px'},600).animate({height: '100%'},"fast");
+
+/* Fire message */
 	var sendres = function() {
-		$(this).animate({height: '0px'},"fast").fadeIn("fast").animate({height: '500px'},"fast").animate({height: '100%'},"fast");
+		$(this).animate({height: '0px'},"fast").fadeIn('fast').animate({height: '500px'},500).animate({height: '100%'},"fast");
 	}
 	var sendready = function() {
 	 //put if...else here
 		//if success
-		$('div#sendsuccess').fadeToggle(1,sendres);
+		$('div#sendsuccess').animate({},100,sendres);
 		//else fail
-		$('div#sendfail').fadeToggle(1,sendres);
+		$('div#sendfail').animate({},100,sendres);
+	}
+	var mfirea = function() {
+		$('#contact').animate({height: '100px'},"fast").fadeIn(800).animate({height: '540px'},600).animate({height: '100%'},"fast");
 	}
 	$('#username').html('example &lt;example@example.com&gt;');
+	$('#contact').animate({height: '100px'},"fast").fadeIn(800).animate({height: '540px'},600).animate({height: '100%'},"fast");
 	//send
 	$("#firemessage").click(function() {
 		$('#fireburn').fadeIn(500).animate({color:"#FF0000"},300);
@@ -55,13 +87,11 @@ $(document).ready(function() {
 	$('#morefire').click(function() {
 		$('#subject').val(null);
 		$('#message').val(null);
-		$('div#sendsuccess').fadeOut("fast");
-		$('#contact').fadeIn("fast").animate({height: '600px'}).animate({height: '100%'},"fast");
+		$('div#sendsuccess').fadeOut("fast",mfirea);
 	});
 	//send fail
 	$('#fireagain').click(function() {
-		$('div#sendfail').fadeOut("fast");
-		$('#contact').fadeIn("fast").animate({height: '600px'}).animate({height: '100%'},"fast");
+		$('div#sendfail').fadeOut("fast",mfirea);
 	});
 
 
--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,33 +1,95 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	/* Long to Short */
-	var fadeshort = function(){$('#fireshout span:last').fadeIn("fast");}
-	var fadelong = function(){$('#fireshout span:first').fadeIn("fast");}
-	$("#fireshout").click(function(){
-		if ($('#fireshout span:last').attr("style")==="display: none;")
+/* Long to Short  index.ver */
+	var fadeshort = function() {
+		$('#fireshout span:last').fadeIn("fast");
+	}
+	var fadelong = function() {
+		$('#fireshout span:first').fadeIn("fast");
+	}
+	var selectshort = function() {
+		$('#shorturl').select();
+	}
+
+	$("#fireshout").click(function() {
+		if ($('#fireshout span:last').attr("style") === "display: none;") 
 			{
-				$("#shorturl").animate({width:"450px"});
-				$("#fireshout").animate({width:"200px"});
-				$('#fireshout span:first').fadeOut("fast",fadeshort);
-				$('#shorturl').attr('placeholder','FFF！');
-			}
-		else
-			{
-				$("#shorturl").animate({width:"820px"});
-				$("#fireshout").animate({width:"450px"});
-				$('#fireshout span:last').fadeOut("fast",fadelong);
-				$('#shorturl').attr('placeholder','烧！烧！烧！');
-			}
+				$("#shorturl").animate({width: "450px"});
+				$("#fireshout").animate({width: "200px"},'normal',selectshort);
+				$('#fireshout span:first').fadeOut("fast", fadeshort);
+				$('#shorturl').attr('readonly', 'readonly').val('http://fff.ac/fire').click(selectshort);
+				// ajax 
+		} 
+		else {
+			$("#shorturl").animate({width: "820px"});
+			$("#fireshout").animate({width: "450px"});
+			$('#fireshout span:last').fadeOut("fast", fadelong);
+			$('#shorturl').attr('placeholder', '烧！烧！烧！').removeAttr("readonly").val(null);
+		}
 	});
 
-	/* Fire message */
-	var firelight = "<span class='glyphicon glyphicon-fire firefire'></span>";       
-	$("#firemessage").click(function(){
-		
-		$("#wrap #contact").animate({height:"0px"});
-		$("#fireshout").animate({width:"200px"});
-		$('#fireshout span:first').fadeOut("fast",fadeshort);
-		$('#shorturl').attr('placeholder','FFF！');
+/* Long to Short  special.ver */
+	var fadeshortsp = function() {
+		$('#fireshoutsp span:last').fadeIn("fast");
+	}
+	var fadelongsp = function() {
+		$('#fireshoutsp span:first').fadeIn("fast");
+	}
+	var selectshortsp = function() {
+		$('#shorturlsp').select();
+	}
+
+	$("#fireshoutsp").click(function() {
+		if ($('#fireshoutsp span:last').attr("style") === "display: none;") 
+			{
+				$("#shorturlsp").animate({width: "450px"});
+				$("#fireshoutsp").animate({width: "200px"},'normal',selectshortsp);
+				$('#fireshoutsp span:first').fadeOut("fast", fadeshortsp);
+				$('#shorturlsp').attr('readonly', 'readonly').val('http://fff.ac/firesp').click(selectshortsp);
+				// ajax 
+		} 
+		else {
+			$("#shorturlsp").animate({width: "820px"});
+			$("#fireshoutsp").animate({width: "450px"});
+			$('#fireshoutsp span:last').fadeOut("fast", fadelongsp);
+			$('#shorturlsp').attr('placeholder', 'F！F！F！').removeAttr("readonly").val(null);
+		}
 	});
 
+/* Fire message */
+	var sendres = function() {
+		$(this).animate({height: '0px'},"fast").fadeIn("fast").animate({height: '500px'},"fast").animate({height: '100%'},"fast");
+	}
+	var sendready = function() {
+	 //put if...else here
+		//if success
+		$('div#sendsuccess').animate({},100,sendres);
+		//else fail
+		$('div#sendfail').animate({},100,sendres);
+	}
+	var mfirea = function() {
+		$('#contact').fadeIn("fast").animate({height: '600px'}).animate({height: '100%'},"fast");
+	}
+	$('#username').html('example &lt;example@example.com&gt;');
+	//send
+	$("#firemessage").click(function() {
+		$('#fireburn').fadeIn(500).animate({color:"#FF0000"},300);
+		$("#contact").animate({height: "0px"},600);
+		$('#fireburn').fadeOut(120);
+		$('#contact').fadeOut(400,sendready);
+	});
+	//send success
+	$('#morefire').click(function() {
+		$('#subject').val(null);
+		$('#message').val(null);
+		$('div#sendsuccess').fadeOut("fast",mfirea);
+	});
+	//send fail
+	$('#fireagain').click(function() {
+		$('div#sendfail').fadeOut("fast",mfirea);
+	});
+
+
+
+//end jquery
 });
